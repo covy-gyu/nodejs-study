d562998ffc2ae28e5af5ea3d34ef4348cc1e4b53
gyucheol Kim <kdjin11@naver.com>
2023-06-23 20:22:25 +0900

    [Add] init

---
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { FaCode } from "react-icons/fa";
+
+function LandingPage() {
+    return (
+        <div style={{width: '100%', margin: '0'}}>
+
+            {/* Main Image */}
+
+            <div style={{width: '85%', margin: '1rem auto'}}>
+
+                <h2>Movies by latest</h2>
+                <hr />
+
+                {/* Moive Grid Cards */}
+
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button> Load More </button>
+            </div>
+
+        </div>
+
+        
+    )
+}
+
+export default LandingPage
